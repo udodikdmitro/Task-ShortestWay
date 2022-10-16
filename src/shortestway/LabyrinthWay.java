62ac6c025c637a59b31d3294295b478b8741bfc8
dima udodik <dima udodik>
2022-10-16 21:02:54 +0300

    Refactoring.

--- src/shortestway/LabyrinthWay.java
+++ src/shortestway/LabyrinthWay.java
@@ -35,7 +35,7 @@ class Pair {
     }
 }
 
-class Main {
+class LabyrinthWay {
     // Ниже массивы детализируют все четыре возможных перемещения из ячейки
     private static final int[] row = {-1, 0, 0, 1};
     private static final int[] col = {0, -1, 1, 0};
@@ -43,16 +43,16 @@ class Main {
     // Функция проверки возможности перехода на позицию (строка, столбец)
     // от текущей позиции. Функция возвращает false, если (строка, столбец)
     // недопустимая позиция или имеет значение 0 или уже посещено.
-    private static boolean isValid(int[][] mat, boolean[][] visited, int row, int col) {
-        return (row >= 0) && (row < mat.length) && (col >= 0) && (col < mat[0].length)
-                && mat[row][col] == 1 && !visited[row][col];
+    private static boolean isValid(int[][] mat, boolean[][] visited, int rowNumber, int colNumber) {
+        return (rowNumber >= 0) && (rowNumber < mat.length) && (colNumber >= 0) && (colNumber < mat[0].length)
+                && mat[rowNumber][colNumber] == 1 && !visited[rowNumber][colNumber];
     }
 
     // Находим кратчайший маршрут в матрице `mat` из источника
     // ячейка (i, j) в ячейку назначения (x, y)
-    private static int findShortestPathLength(int[][] mat, int i, int j, int x, int y) {
+    public static int findShortestPathLength(int[][] mat, int currentX, int currentY, int distanationX, int distanationY) {
         // базовый случай: неверный ввод
-        if (mat == null || mat.length == 0 || mat[i][j] == 0 || mat[x][y] == 0) {
+        if (mat == null || mat.length == 0 || mat[currentX][currentY] == 0 || mat[distanationX][distanationY] == 0) {
             throw new IllegalArgumentException("Wrong coordinates");
         }
 
@@ -67,8 +67,8 @@ class Main {
         Queue<Point> q = new ArrayDeque<>();
 
         // помечаем исходную ячейку как посещенную и ставим исходный узел в queue
-        visited[i][j] = true;
-        q.add(new Point(i, j, new ArrayList<>()));
+        visited[currentX][currentY] = true;
+        q.add(new Point(currentX, currentY, new ArrayList<>()));
         // сохраняет длину самого длинного пути от источника к месту назначения
         Integer min_dist = null;
 
@@ -79,12 +79,11 @@ class Main {
 
             // (i, j) представляет текущую ячейку, а `dist` хранит ее
             // минимальное расстояние от источника
-            i = currentPoint.x;
-            j = currentPoint.y;
-            int dist = currentPoint.dist;
+            currentX = currentPoint.x;
+            currentY = currentPoint.y;
 
             // если пункт назначения найден, обновляем `min_dist` и останавливаемся
-            if (currentPoint.x == x && currentPoint.y == y) {
+            if (currentX == distanationX && currentY == distanationY) {
                 min_dist = currentPoint.dist;
                 System.out.println(currentPoint.path);
                 break;
@@ -94,11 +93,11 @@ class Main {
             // и ставим в queue каждое допустимое движение
             for (int k = 0; k < 4; k++) {
                 // проверяем, можно ли выйти на позицию
-                // (i + row[k], j + col[k]) от текущей позиции
-                if (isValid(mat, visited, i + row[k], j + col[k])) {
+                // (currentX + row[k], currentY + col[k]) от текущей позиции
+                if (isValid(mat, visited, currentX + row[k], currentY + col[k])) {
                     // отметить следующую ячейку как посещенную и поставить ее в queue
-                    visited[i + row[k]][j + col[k]] = true;
-                    q.add(new Point(i + row[k], j + col[k], currentPoint.path));
+                    visited[currentX + row[k]][currentY + col[k]] = true;
+                    q.add(new Point(currentX + row[k], currentY + col[k], currentPoint.path));
                 }
             }
         }
@@ -110,31 +109,5 @@ class Main {
             throw new RuntimeException("There is no way");
         }
 
-    }
-
-
-    public static void main(String[] args) {
-        int[][] mat =
-                {
-                        {1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
-                        {0, 1, 1, 1, 1, 1, 0, 1, 0, 1},
-                        {0, 0, 1, 0, 1, 1, 1, 0, 0, 1},
-                        {1, 0, 1, 1, 1, 0, 1, 1, 0, 1},
-                        {0, 0, 0, 1, 0, 0, 0, 1, 0, 1},
-                        {1, 0, 1, 1, 1, 0, 0, 1, 1, 0},
-                        {0, 0, 0, 0, 1, 0, 0, 1, 0, 1},
-                        {0, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-                        {1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
-                        {0, 0, 1, 0, 0, 1, 1, 0, 0, 1},
-                };
-
-        int min_dist = findShortestPathLength(mat, 0, 0, 7, 5);
-
-        if (min_dist != -1) {
-            System.out.println("The shortest path from source to destination " +
-                    "has length " + min_dist);
-        } else {
-            System.out.println("Destination cannot be reached from source");
-        }
     }
 }
---
+++ src/shortestway/Main.java
@@ -0,0 +1,26 @@
+package shortestway;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] mat =
+                {
+                        {1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+                        {0, 1, 1, 1, 1, 1, 0, 1, 0, 1},
+                        {0, 0, 1, 0, 1, 1, 1, 0, 0, 1},
+                        {1, 0, 1, 1, 1, 0, 1, 1, 0, 1},
+                        {0, 0, 0, 1, 0, 0, 0, 1, 0, 1},
+                        {1, 0, 1, 1, 1, 0, 0, 1, 1, 0},
+                        {0, 0, 0, 0, 1, 0, 0, 1, 0, 1},
+                        {0, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+                        {1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+                        {0, 0, 1, 0, 0, 1, 1, 0, 0, 1},
+                };
+
+        LabyrinthWay labyrinthWay = new LabyrinthWay();
+
+        int min_dist = labyrinthWay.findShortestPathLength(mat, 0, 0, 7, 5);
+
+            System.out.println("The shortest path from source to destination " +
+                    "has length " + min_dist);
+    }
+}
